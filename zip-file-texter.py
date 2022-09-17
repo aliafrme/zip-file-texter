0f5736ab5ae4dd14c40a067340859fed88780ddd
aliafrme <111320610+aliafrme@users.noreply.github.com>
2022-09-17 21:16:49 +0430

    Create zip-file-texter.py

---
+++ zip-file-texter.py
@@ -0,0 +1,70 @@
+import os
+import zipfile
+import tkinter as tk
+from tkinter.filedialog import askopenfilename, askdirectory
+from tkinter import END, Label, messagebox
+
+
+window = tk.Tk(className='Python Examples - Window Color')
+window.minsize(300,300)
+window.maxsize(500,500)
+window.title("zip file texter")
+tk.Entry(window, justify='center')
+
+def open_file():
+    filename = askopenfilename(title="Select a zip file",filetypes = (("Zip file", "*.zip"),("All files", "*.*")))
+    try:
+        with zipfile.ZipFile(filename, mode="r") as archive:
+            # archive.printdir()
+            lbl_file_address["text"] = filename
+    except:
+        print("file not found")
+
+def extract_file():
+    save_address = askdirectory()
+    lbl_destination_address["text"] = save_address
+
+    if ent_folder_name.get() != "":
+        file_name = ent_folder_name.get()
+    else:
+        file_name = os.path.basename(lbl_file_address["text"]).split(".")[0]
+    print(file_name)
+    dest_address = os.path.join(save_address, file_name)
+    
+    try:
+        with zipfile.ZipFile(lbl_file_address["text"], mode="r") as archive:
+            # archive.printdir()
+            archive.extractall(dest_address)
+    except:
+        print("Can't extract file")
+
+menubar = tk.Menu(window)
+file = tk.Menu(menubar, tearoff = 0)
+menubar.add_cascade(label ='File', menu = file)
+file.add_command(label ='Open', command = open_file)
+
+# open_btn = tk.Button(window, text="Open" ,command=open_file)
+# open_btn.grid(row=0,)
+
+extract_btn = tk.Button(window, text="Extract" ,command=extract_file)
+extract_btn.grid(row=0,)
+extract_btn.configure(bg='#fff')
+lbl_file_address = tk.Label(window)
+lbl_destination_address = tk.Label(window)
+
+
+lbl_destination_address.grid(row=2)
+lbl_file_address.configure(bg='#fff')
+
+lbl_extract = Label(window, text="Enter folder name :  ")
+lbl_extract.grid(row=3)
+lbl_extract.configure(bg='#55f6c0')
+ent_folder_name = tk.Entry(window)
+# ent_folder_name.insert(0, 'Enter folder name')
+ent_folder_name.grid(row=4)
+
+extract_btn.grid(row=5)
+
+window.config(menu = menubar)
+window.configure(bg='#55f6c0')
+window.mainloop()
